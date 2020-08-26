89148932b7f4e29a7a35417018fa8a3d8ec334f2
Xavier Olivares <xm.olivares@gmail.com>
2020-08-25 20:42:02 -0400

    interesting use of prevState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,16 +10,35 @@ class Counter extends Component {
     }
     increment() {
         // never modify state directly. React will not rerender the state that way. Use setState to modify it
-        this.setState({
-            count: this.state.count+1
-        })
+        // calls to setState are asynchronous
+        // this.setState({
+        //     count: this.state.count+1
+        // }, () => console.log('callback value - state count', this.state.count) );
+
+        // fix
+        this.setState((prevState, props) => ({
+            count: prevState.count + 1
+        }))
+
+        // calls to console log are happening synchronously
+        console.log('synchronous count', this.state.count)
+    }
+
+    incrementFive() {
+        // React may update the calls into a single update for better performance.
+        // All five setState calls are done in a singleGo.
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
 
     render() {
         return (
             <div>
                 <h1>Count - {this.state.count}</h1>
-                <button onClick={()=> this.increment()}>Increment</button>
+                <button onClick={()=> this.incrementFive()}>Increment</button>
             </div>
         )
     }
