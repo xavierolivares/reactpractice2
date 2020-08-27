77c3f06fed6a0afb24b901fbdac2519dfb12e4dd
Xavier Olivares <xm.olivares@gmail.com>
2020-08-26 20:48:58 -0400

    map practice with object data

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diana']
-    const nameList = names.map((name, i) => <h2 key={i} >{name}</h2>)
+    const persons = [
+        {
+          name: "Bruce",
+          id: 1,
+          age: 30,
+          skill: "React"
+        },
+        {
+          name: "Clark",
+          id: 2,
+          age: 25,
+          skill: "Angular"
+        },
+        {
+          name: "Diana",
+          id: 3,
+          age: 28,
+          skill: "Vue"
+        }
+    ]
+    const personList = persons.map((person) => <h2 key={person.id} >I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>)
     return (
         <div>
             {/* <h2>{names[0]}</h2>
             <h2>{names[1]}</h2>
             <h2>{names[2]}</h2> */}
-            {nameList}
+            {personList}
         </div>
     )
 }
