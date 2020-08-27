05b6290b469870704dcc57ebee3430091cab4916
Xavier Olivares <xm.olivares@gmail.com>
2020-08-26 20:57:08 -0400

    list component needs to contain the key prop, because it is doing the mapping

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -22,7 +22,7 @@ function NameList() {
           skill: "Vue"
         }
     ]
-    const personList = persons.map((person) => <Person person={person} />)
+    const personList = persons.map((person) => <Person key={person.id} person={person} />)
     return (
         <div>
             {/* <h2>{names[0]}</h2>
--- src/components/Person.js
+++ src/components/Person.js
@@ -5,7 +5,7 @@ function Person(props) {
     console.log(person)
     return (
         <div>
-            <h2 key={person.id} >I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>
+            <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>
         </div>
     )
 }
