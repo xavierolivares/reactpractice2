d6500ea153e34896c098acd133e86d4b2c5ff328
Xavier Olivares <xm.olivares@gmail.com>
2020-08-30 23:29:21 -0400

    componentDidCatch takes error and info params

--- src/App.js
+++ src/App.js
@@ -34,7 +34,11 @@ function App() {
     <div className="App">
       <ErrorBoundary>
         <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
         <Hero heroName="Superman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
         <Hero heroName="Joker" />
       </ErrorBoundary>
       {/* <PortalDemo /> */}
