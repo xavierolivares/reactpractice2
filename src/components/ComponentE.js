3a586c2a457445360869c71c5e9e6079c5340333
Xavier Olivares <xm.olivares@gmail.com>
2020-09-02 21:06:02 -0400

    practice with contextType in ComponentE

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 export class ComponentE extends Component {
     render() {
         return (
             <div>
+                Component E Context {this.context}
                 <ComponentF />
             </div>
         )
     }
 }
+
+ComponentE.contextType = UserContext
 
 export default ComponentE
